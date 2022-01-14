16e5fd9516469ef8bed98e20b2710985735591db
Garrett Anderson <keaton85@runbox.com>
2022-01-14 16:17:05 -0600

    add fade effect to images

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let overlay=document.querySelectorAll(".card-img-overlay");
 
 overlay.forEach(function(element) {
     element.addEventListener("mouseenter",function() {
+element.classList.remove("fade-out");
+        element.classList.add("fade-in");
         element.style.opacity=1;
     })
     element.addEventListener("mouseleave",function() {
+        element.classList.remove("fade-in");
+        element.classList.add("fade-out");
         element.style.opacity=0;
     })
 })
