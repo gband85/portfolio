cb115ba5c60edfa335a6c67ea603cb81cf6aa809
gband85 <garrett@gband.net>
2022-06-30 16:36:01 -0500

    add image import function

--- src/page.js
+++ src/page.js
@@ -1,5 +1,19 @@
 import "./page.css";
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("./images", false, /\.(png|jpe?g|svg)$/)
+);
+
+
+
 const page = () => {
   const hero = document.createElement("div");
   hero.className = "hero";
