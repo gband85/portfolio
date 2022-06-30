ac12a597a5c9db0c5980d075a8011b8bf71eaabe
gband85 <garrett@gband.net>
2022-06-30 16:05:08 -0500

    create and attach navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //const {navbar}=window.Navbar
-// import { navbar } from "./navbar";
+ import { navbar } from "./navbar";
 // import {page} from "./page";
 //create dropdown menu
-// const drop=navbar("FreeJS","Home","Work");
+ const drop=navbar("FreeJS","Home","Work");
 //add elements to page
-// document.body.appendChild(drop.init());
+ document.body.appendChild(drop.init());
 // document.body.appendChild(page().hero);
 // document.body.appendChild(page().container);
 //drop.init()
