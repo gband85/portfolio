93ebf56dbd260eb2ec24c6cbed6b9ef83860b35d
gband85 <garrett@gband.net>
2022-06-30 14:20:18 -0500

    add hero

---
+++ src/page.js
@@ -0,0 +1,11 @@
+const page = () => {
+    const hero = document.createElement("div");
+    hero.className = "hero";
+    hero.innerHTML = `<div class="hero__text">
+    <h1 class="">Garrett Anderson</h1>
+    <h2 class="">Developer</h2>
+  </div>`;
+
+return {hero}
+}
+export {page}
