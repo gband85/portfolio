d7dd4f8f0796039e97dc4b7fefb3efe766b6a0ef
gband85 <garrett@gband.net>
2022-07-05 12:28:08 -0500

    add more links to navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
  import { navbar } from "./navbar";
  import {page} from "./page";
 //create dropdown menu
- const drop=navbar("FreeJS","Home","Work");
-//add elements to page
- document.body.appendChild(drop.init());
+ const drop=navbar("GBA","Home","About","Work","Contact");//add elements to page
+ const nav=drop.init();
+ document.body.appendChild(nav);
  document.body.appendChild(page().hero);
-// document.body.appendChild(page().container);
-//drop.init()
-//import './page';
+ document.body.appendChild(page().container);
