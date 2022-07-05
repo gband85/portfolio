81731e5e3bba8eb839e6c461f6cd147a2d3f8bf8
gband85 <garrett@gband.net>
2022-07-05 11:41:35 -0500

    remove svg path

--- src/navbar.js
+++ src/navbar.js
@@ -19,20 +19,11 @@ const navbar = (brandText, ...links) => {
     const hamburger = document.createElement(
       'button',
     );
-    const hamburgerPath = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'path',
-    );
-    hamburger.classList.add('hamburger');
+     hamburger.classList.add('hamburger');
     hamburger.style.width = '24px';
     hamburger.style.height = '24px';
     hamburger.setAttribute('viewBox', '0 0 24 24');
-    hamburgerPath.setAttribute(
-      'd',
-      'M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z',
-    );
-    hamburgerPath.setAttribute('fill', 'currentColor');
-    hamburger.appendChild(hamburgerPath);
+    hamburger.appendChild();
     navButtons.appendChild(brand);
     navButtons.appendChild(hamburger);
     const navList = document.createElement('ul');
