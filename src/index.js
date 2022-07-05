991c0d595a626812dda23240b4bb374d304e470b
gband85 <garrett@gband.net>
2022-07-05 13:14:38 -0500

    remove link from navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  import { navbar } from "./navbar";
  import {page} from "./page";
 //create dropdown menu
- const drop=navbar("GBA","Home","About","Work","Contact");//add elements to page
+ const drop=navbar("GBA","About","Work","Contact");//add elements to page
  const nav=drop.init();
  document.body.appendChild(nav);
  document.body.appendChild(page().hero);
