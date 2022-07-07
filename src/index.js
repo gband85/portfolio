6074573798187b287e1b8189ba60e86861d37484
gband85 <garrett@gband.net>
2022-07-06 20:12:09 -0500

    add overlay

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 //create dropdown menu
  const drop=navbar("GBA","About","Work","Contact");//add elements to page
  const nav=drop.init();
+ document.body.appendChild(page().overlay);
+ document.body.querySelector('.overlay').style.display="none"
  document.body.appendChild(nav);
  document.body.appendChild(page().hero);
  document.body.appendChild(page().container);
