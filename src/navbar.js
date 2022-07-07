1be1a84950da38a6446261cde55ae3c42780ffc0
gband85 <garrett@gband.net>
2022-07-07 11:43:58 -0500

    add navMenu

--- src/navbar.js
+++ src/navbar.js
@@ -7,6 +7,32 @@ const pageNav = (brandText, ...links) => {
   //accept any args
   //first arg is logo text,1 to end is buttons text
   let linksArr = links.slice(0);
+  const navMenu=() => {
+    const navMenu=  document.createElement('div');
+    navMenu.className='nav__menu';
+    const navList = document.createElement('ul');
+    navList.className = 'nav__menu__list';
+    //iterate through args,create li with button with arg
+    linksArr.map(function (item) {
+      const listItem = document.createElement('li');
+      listItem.className = 'menu-item';
+      const navLink=document.createElement('a')
+      navLink.className="nav-link"
+      navLink.textContent= item;
+      navLink.addEventListener('click',function(){
+        navList.classList.toggle('show');
+        document.body.querySelector('.overlay').style.display="none";
+      })
+      listItem.appendChild(navLink);
+      navList.appendChild(listItem);
+    });
+    console.log(navList.firstChild)
+    navList.firstChild.firstChild.href="index.html#navbar"
+    navList.firstChild.nextSibling.firstChild.href="index.html#projects-section"
+    navList.firstChild.nextSibling.nextSibling.firstChild.href="index.html#contact-section"
+  navMenu.appendChild(navList);
+  return navMenu;
+    }
   const navbar = () => {
     const navbar=document.createElement('div');
     navbar.className="navbar";
