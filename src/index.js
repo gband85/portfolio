39d9f682edff7e3f3e53a647385e8d99cf6c8395
gband85 <garrett@gband.net>
2022-07-07 16:08:49 -0500

    add main and header sections

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 //const {navbar}=window.Navbar
- import { navbar } from "./navbar";
+ import { pageNav } from "./navbar";
  import {page} from "./page";
+ 
+
 //create dropdown menu
- const drop=navbar("GBA","About","Work","Contact");//add elements to page
- const nav=drop.init();
+ const drop=pageNav("GBA","About","Work","Contact");//add elements to page
+ const navbar=drop.navbar();
+ const navMenu=drop.navMenu();
  document.body.appendChild(page().overlay);
- document.body.querySelector('.overlay').style.display="none"
- document.body.appendChild(nav);
- document.body.appendChild(page().hero);
- document.body.appendChild(page().container);
+ document.body.querySelector('.overlay').style.display="none";
+ const header=document.createElement('header');
+ header.appendChild(navMenu); 
+ header.appendChild(navbar);
+ document.body.appendChild(header);
+ const main=document.createElement('main'); 
+ main.appendChild(page().hero);
+ main.appendChild(page().container);
+ document.body.appendChild(main);
