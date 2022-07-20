01d43de4df7d8194def1ca35bcd101fabed35ef2
gband85 <garrett@gband.net>
2022-07-19 19:35:17 -0500

    adjust asset output

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,16 @@ module.exports = {
 
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
       },
     ],
   },
