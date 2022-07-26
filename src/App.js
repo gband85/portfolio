66678fb2d3609ca99ca4a4ac1917eaff9121f2f1
gband85 <garrett@gband.net>
2022-07-25 19:19:13 -0500

    configure files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    <Page/>
+    </div>
+  );
+}
+
+export default App;
