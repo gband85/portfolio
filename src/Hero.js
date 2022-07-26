9a94d63dd33d3b36e198740bc8d8e2cc6fadb013
gband85 <garrett@gband.net>
2022-07-26 10:17:18 -0500

    add hero

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+const Hero = ()=>{
+    return (
+    <div className = 'hero' id = 'hero'>
+<div class="hero__text">
+  <h1 class="hero__title">Garrett Anderson</h1>
+  <h2 class="hero__subtitle">Developer</h2>
+</div>
+</div>
+    )
+}
+
+export default Hero;
