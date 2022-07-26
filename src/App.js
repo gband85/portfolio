011ad035385eafbaac4c0ee07cda97edc7221a2d
gband85 <garrett@gband.net>
2022-07-26 11:45:13 -0500

    import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Hero from './Hero';
+import Page from './Page';
 
 function App() {
   return (
     <div>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Hero/>
     <Page/>
     </div>
