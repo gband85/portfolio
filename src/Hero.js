7cc9a5d37a7db6618ddf14add71521e5af2ea66e
gband85 <garrett@gband.net>
2022-10-06 15:51:29 -0500

    fix typos

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,9 @@
 const Hero = ()=>{
     return (
     <div className = 'hero' id = 'hero'>
-<div class="hero__text">
-  <h1 class="hero__title">Garrett Anderson</h1>
-  <h2 class="hero__subtitle">Developer</h2>
+<div className="hero__text">
+  <h1 className="hero__title">Garrett Anderson</h1>
+  <h2 className="hero__subtitle">Developer</h2>
 </div>
 </div>
     )
